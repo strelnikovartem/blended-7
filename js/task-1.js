7ca3ef1f4faee99735483b051a115f0b6bc5d7dc
Artem Strelnikov <piramida1237@gmail.com>
2026-01-04 20:40:49 +0100

    add_tack1-10

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,12 @@
-console.log("ok");
+const body = document.querySelector("body");
+const titleEl = document.querySelector("#title");
+const listEl = document.querySelector(".list");
+const allDatTopic = document.querySelectorAll("[data-topic]");
+const nextEl = titleEl.nextElementSibling;
+const allHEl = document.querySelectorAll("h3");
+
+allHEl.forEach((item) => item.classList.add("active"));
+
+const liEl = document.querySelector("[data-topic='navigation']");
+
+console.log(liEl);
