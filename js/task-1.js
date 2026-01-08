68f1dcdf3d47950332fbf722d10c25f73789a058
Artem Strelnikov <piramida1237@gmail.com>
2026-01-08 20:07:26 +0100

    all_task_work-1

--- js/task-1.js
+++ js/task-1.js
@@ -8,5 +8,51 @@ const allHEl = document.querySelectorAll("h3");
 allHEl.forEach((item) => item.classList.add("active"));
 
 const liEl = document.querySelector("[data-topic='navigation']");
+liEl.style.backgroundColor = "yellow";
 
-console.log(liEl);
+const pEl = liEl.querySelector("p");
+
+const currentTopic = "manipulation";
+
+const newLiEl = document.querySelector(`[data-topic=${currentTopic}`);
+
+const blue = (newLiEl.style.backgroundColor = "blue");
+
+const hEl = document.querySelector("h3.completed");
+
+const remuveEl = hEl.remove();
+
+const newEl = document.createElement("p");
+newEl.textContent = "Об'єктна модель документа (Document Object Model)";
+
+titleEl.append(newEl);
+
+const newItemTitle = document.createElement("h3");
+newItemTitle.textContent = "Властивість innerHTML";
+
+const newItemText = document.createElement("p");
+newItemText.textContent =
+  "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу";
+
+newItemTitle.append(newItemText);
+
+listEl.append(newItemTitle);
+
+const topicsEl = document.querySelectorAll("[data-topic]");
+
+topicsEl.forEach((li) => {
+  if (li.dataset.topic === "props") {
+    li.style.color = "orange";
+  }
+});
+
+const markup = `
+<li>
+          <h3>Властивість innerHTML</h3>
+          <p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу???</p>
+        </li>
+`;
+
+listEl.insertAdjacentHTML("beforeend", markup);
+
+// console.log(topicsEl);
---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+const container = document.querySelector(".number-container");
+
+const arr = [];
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 0) {
+    arr.push(i.style.);
+  }
+  return arr;
+}
