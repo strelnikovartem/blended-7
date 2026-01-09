6c0852e25ef2fe8cc8106ec02ef45c1c60126f5f
Artem Strelnikov <piramida1237@gmail.com>
2026-01-09 20:01:22 +0100

    add task-6, html, css, js

--- js/task-5.js
+++ js/task-5.js
@@ -1,1 +1,13 @@
 const container = document.querySelector(".container-box");
+
+[...container.children].map((box) => {
+  box.addEventListener("click", handleClick);
+});
+
+function handleClick(event) {
+  if (!event.target.classList.contains("box")) {
+    return;
+  }
+  const color = event.target.dataset.color;
+  console.log(color);
+}
