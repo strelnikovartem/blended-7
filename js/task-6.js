bb364e668f6f7154f6c4c756c7246cb3a4708e35
Artem Strelnikov <piramida1237@gmail.com>
2026-01-10 21:06:13 +0100

    add createMarkup

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,49 @@
+const products = [
+  {
+    id: 1,
+    img: "https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG",
+    name: "Monitor",
+    price: 3000,
+    description: "23-inch monitor with Full HD resolution.",
+  },
+  {
+    id: 3,
+    img: "https://cdn.27.ua/799/66/39/6841913_1.jpeg",
+    name: "Smartphone",
+    price: 8000,
+    description: "Equipped with a triple camera and a multi-core processor.",
+  },
+  {
+    id: 4,
+    img: "https://cdn.27.ua/799/b6/16/4371990_1.jpeg",
+    name: "Tablet",
+    price: 12000,
+    description: "10-inch tablet with high performance and a Retina display.",
+  },
+];
 
+const container = document.querySelector(".products");
+
+container.insertAdjacentHTML("beforeend", createMarkup(products));
+container.addEventListener("click", handleClick);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      (item) => `
+     <li data-id="${item.id}" class="item product-item">
+        <img src="${item.img}" alt="${item.name}" width='300'>
+        <h2>${item.name}</h2>
+        <p>Coast: ${item.price} $</p>
+    </li>
+    `
+    )
+    .join("");
+}
+
+function handleClick(event) {
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  console.log(event.target);
+}
