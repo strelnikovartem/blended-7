e6456cbea4d19baf0d567a4939d729ba2d0b01c1
Artem Strelnikov <piramida1237@gmail.com>
2026-01-13 19:40:46 +0100

    next qwest

--- js/task-5.js
+++ js/task-5.js
@@ -1,8 +1,18 @@
 const container = document.querySelector(".container-box");
 
-[...container.children].map((box) => {
-  box.addEventListener("click", handleClick);
-});
+// [...container.children].map((box) => {
+//   box.addEventListener("click", handleClick);
+// });
+
+// function handleClick(event) {
+//   if (!event.target.classList.contains("box")) {
+//     return;
+//   }
+//   const color = event.target.dataset.color;
+//   console.log(color);
+// }
+
+container.addEventListener("click", handleClick);
 
 function handleClick(event) {
   if (!event.target.classList.contains("box")) {
